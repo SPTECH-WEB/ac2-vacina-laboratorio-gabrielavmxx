315f6ae6cb2fe92667323a15be75b80e617d3c5a
gabrielavmxx <gabriela.montero@sptech.school>
2025-10-22 13:38:32 -0300

    Prova ac2

--- src/main/java/sptech/school/Laboratorio.java
+++ src/main/java/sptech/school/Laboratorio.java
@@ -1,5 +1,110 @@
-package school.sptech;
+package sptech.school;
+
+import sptech.school.exception.ArgumentoInvalidoException;
+import sptech.school.exception.VacinaInvalidaException;
+import sptech.school.exception.VacinaNaoEncontradaException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Laboratorio {
+    private String nome;
+    private List<Vacina> vacinas;
 
+    public Laboratorio(String nome) {
+        this.nome = nome;
+        this.vacinas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Vacina> getVacinas() {
+        return vacinas;
+    }
+
+    public void adicionarVacina(Vacina vacina){
+            if(vacina == null && vacina.getNome() == null && vacina.getTipo() == null && vacina.getPreco() == null && vacina.getPreco() < 0.0 && vacina.getEficacia() <0 && vacina.getEficacia() > 5 && vacina.getDataLancamento() == null && vacina.getDataLancamento().isBefore(LocalDate.now())){
+                throw new VacinaInvalidaException();
+            }else{
+                vacinas.add(vacina);
+                System.out.println("Vacina adicionada com sucesso!!!!");
+            }
+    }
+
+    public Vacina buscarVacinaPorCodigo(String codigo){
+            if(codigo == null && codigo == ""){
+                throw new VacinaNaoEncontradaException();
+            }else{
+                for(Vacina vacina: vacinas){
+                    if(codigo.equalsIgnoreCase(vacina.getCodigo())){
+                        System.out.println("Vacina encontrada:");
+                        System.out.println(vacina.toString());
+                        return vacina;
+                        }
+            }
+        }
+        return null;
+    }
+
+    public void removerVacinaPorCodigo(String codigo){
+            if(codigo == null && codigo == ""){
+                throw new VacinaNaoEncontradaException();
+            }else{
+                for(Vacina vac : vacinas){
+                    if(codigo.equalsIgnoreCase(vac.getCodigo())){
+                        vacinas.remove(vac);
+                        System.out.println(vac.toString());
+                        System.out.println("Vacina removida com sucesso!!!!");
+                    }
+                }
+            }
+    }
+
+    public Vacina buscarVacinaComMelhorEficacia(){
+            if(vacinas == null){
+                throw new VacinaNaoEncontradaException();
+            }else{
+                Vacina maiorEficacia = new Vacina();
+                maiorEficacia.setEficacia(0.0);
+                for(Vacina vacina : vacinas){
+                    if(maiorEficacia.getEficacia() < vacina.getEficacia()){
+                        maiorEficacia = vacina;
+                    } else if (maiorEficacia.getEficacia().equals(vacina.getEficacia())){
+                        if(maiorEficacia.getDataLancamento().isAfter(vacina.getDataLancamento())){
+
+                        }else{
+                            maiorEficacia = vacina;
+                        }
+                    }
+                }
+                System.out.println("Vacina com maior eficacia:");
+                System.out.println(maiorEficacia.toString());
+                return maiorEficacia;
+            }
+    }
+
+    public List<Vacina> buscarVacinaPorPeriodo(LocalDate dataInicio, LocalDate dataFim){
+        List<Vacina> vacinaPeriodo = new ArrayList<>();
+           if(dataInicio != null && dataFim != null && dataInicio.isBefore(dataFim)){
+               for(Vacina vacina : vacinas){
+                   if(vacina.getDataLancamento().isAfter(dataInicio) && vacina.getDataLancamento().isBefore(dataFim)){
+                       vacinaPeriodo.add(vacina);
+                   }
+               }
+               System.out.println("Vacinas encontradas dentro do periodo:");
+               for(Vacina vacina : vacinaPeriodo){
+                   System.out.println(vacina.toString());
+               }
+               return vacinaPeriodo;
+           }else{
+               throw new ArgumentoInvalidoException();
+           }
+    }
 }
--- src/main/java/sptech/school/Main.java
+++ src/main/java/sptech/school/Main.java
@@ -1,7 +1,34 @@
-package school.sptech;
+package sptech.school;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Você pode usar essa classe para testar o seu código se precisar.");
+
+        Vacina vacina1 = new Vacina("ABC","Febre Amarela", "A", 29.99, 2.0, LocalDate.of(2025,10,2));
+        Vacina vacina2 = new Vacina("DFG","Gripe", "B", 15.99, 4.9, LocalDate.of(2025,9,1));
+        Vacina vacina3 = new Vacina("HIJ","Hapatite", "C", 12.99, 4.3, LocalDate.of(2025,10,6));
+        Vacina vacina4 = new Vacina("KLM","Tetano", "D", 33.99, 2.9, LocalDate.of(2025,8,2));
+
+        Laboratorio laboratorio = new Laboratorio("Laboratório triste");
+
+        System.out.println("\n\nADICIONAR VACINAS");
+        laboratorio.adicionarVacina(vacina1);
+        laboratorio.adicionarVacina(vacina2);
+        laboratorio.adicionarVacina(vacina3);
+        laboratorio.adicionarVacina(vacina4);
+
+        System.out.println("\n\nBUSCAR POR CODIGO");
+        laboratorio.buscarVacinaPorCodigo("DFG");
+
+        System.out.println("\n\nVACINA COM MELHOR EFICACIA");
+        laboratorio.buscarVacinaComMelhorEficacia();
+
+        System.out.println("\n\nVACINA POR PERIODO");
+        laboratorio.buscarVacinaPorPeriodo(LocalDate.of(2025,10,1), LocalDate.of(2025,10,22));
+
+        System.out.println("\n\nREMOVER VACINA POR CODIGO");
+        laboratorio.removerVacinaPorCodigo("KLM");
     }
 }
